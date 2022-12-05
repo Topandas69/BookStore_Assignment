ae683803d2a7b37c9111c1dc528628426e5fe4a3
Topandas69 <105999599+Topandas69@users.noreply.github.com>
2022-12-05 00:12:48 -0500

    fixed typos

--- TopansBookStore/Areas/Admin/Controllers/ProductController.cs
+++ TopansBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -57,7 +57,7 @@ namespace TopansBookStore.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category, CoverType");
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = allObj });
         }
 
